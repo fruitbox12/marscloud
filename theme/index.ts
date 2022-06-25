b3e0192dce674ab89e167972f022a48c12268465
Natalia Fomina <fominatasha@gmail.com>
2022-06-26 00:00:42 +0200

    Add text and accordion to theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,8 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
+import Text from "./components/text";
+import Accordion from "./components/accordion";
 
 const theme = extendTheme({
   colors,
+  components: {
+    Text,
+    Accordion,
+  },
 });
 
 export default theme;
