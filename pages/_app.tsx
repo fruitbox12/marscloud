a71e2d9550350f0b255ef66a33f3b7ead04e4a29
Natalia Fomina <fominatasha@gmail.com>
2022-06-25 22:08:36 +0200

    Use getLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import { ReactElement, ReactNode } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    <ChakraProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
