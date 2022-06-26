82947c1814a7cc5f6dca20d04c78a4713a4eeff4
Natalia Fomina <fominatasha@gmail.com>
2022-06-26 17:12:56 +0200

    Add link in theme

--- theme/index.ts
+++ theme/index.ts
@@ -2,12 +2,14 @@ import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
 import Text from "./components/text";
 import Accordion from "./components/accordion";
+import Link from "./components/link";
 
 const theme = extendTheme({
   colors,
   components: {
     Text,
     Accordion,
+    Link,
   },
 });
 
