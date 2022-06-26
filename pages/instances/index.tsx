d5c2a118d839394160a54f27c4e5cf4f4aa8bd6a
Natalia Fomina <fominatasha@gmail.com>
2022-06-26 19:34:42 +0200

    Add placeholder

--- pages/instances/index.tsx
+++ pages/instances/index.tsx
@@ -1,9 +1,32 @@
 import { ReactElement } from "react";
+import {
+  Heading,
+  Stack,
+  Text,
+  Button,
+  Image,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
 import Layout from "../../components/layout";
 import type { NextPageWithLayout } from "../_app";
+import createImage from "../../public/create-instance.jpg";
 
 const InstancesPage: NextPageWithLayout = () => {
-  return <p>Instances</p>;
+  return (
+    <Stack spacing={10}>
+      <Heading as="h1">Instances</Heading>
+      <Flex direction="column" alignItems="center">
+        <Box boxSize="sm">
+          <Image src={createImage.src} alt="create instance" />
+        </Box>
+        <Text>Looks like you don’t have any Droplets.</Text>
+        <Button href="/instances/new" as="a" colorScheme="pink" mt={10}>
+          Create instance
+        </Button>
+      </Flex>
+    </Stack>
+  );
 };
 
 InstancesPage.getLayout = function getLayout(page: ReactElement) {
