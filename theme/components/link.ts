55417090503de1fc001aef8919d768975ced683e
Natalia Fomina <fominatasha@gmail.com>
2022-06-26 17:12:15 +0200

    Create variant sidebar for links

---
+++ theme/components/link.ts
@@ -0,0 +1,12 @@
+const link = {
+  variants: {
+    sidebar: {
+      color: "gray.200",
+      py: "8px",
+      px: "16px",
+      transition: "300ms ease-out",
+    },
+  },
+};
+
+export default link;
