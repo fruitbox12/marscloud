b41d02437f1f1ad648691196c78b87115155bdff
Natalia Fomina <fominatasha@gmail.com>
2022-06-26 17:14:23 +0200

    Create instance page

---
+++ pages/instances/[ids].tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react";
+import { useRouter } from "next/router";
+import { Heading } from "@chakra-ui/react";
+import Layout from "../../components/layout";
+import type { NextPageWithLayout } from "../_app";
+
+const InstancePage: NextPageWithLayout = () => {
+  const router = useRouter();
+  const { ids } = router.query;
+
+  return (
+    <>
+      <Heading>Instance {ids}</Heading>
+    </>
+  );
+};
+
+InstancePage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default InstancePage;
