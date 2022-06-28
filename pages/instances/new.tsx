8fb95bd40170f9e4b0a2c04cfee5cd15204362e3
Natalia Fomina <fominatasha@gmail.com>
2022-06-28 06:19:50 +0200

    Use selectImage and selectPlan components

--- pages/instances/new.tsx
+++ pages/instances/new.tsx
@@ -1,14 +1,16 @@
 import { ReactElement } from "react";
-import { useRouter } from "next/router";
-import { Heading } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import Layout from "../../components/layout";
 import type { NextPageWithLayout } from "../_app";
+import SelectImage from "../../components/select-image";
+import SelectPlan from "../../components/select-plan";
 
 const NewInstancePage: NextPageWithLayout = () => {
   return (
-    <>
-      <Heading>Create New Instance</Heading>
-    </>
+    <Stack spacing={16}>
+      <SelectImage />
+      <SelectPlan />
+    </Stack>
   );
 };
 
